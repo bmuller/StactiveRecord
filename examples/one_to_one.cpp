19b942f8ea99142c5c20765545fa658810e2b3c2
Brian Muller <bamuller@gmail.com>
2007-12-18 04:14:10 +0000

    working on one_to_one relationship

--- examples/one_to_one.cpp
+++ examples/one_to_one.cpp
@@ -2,172 +2,88 @@
 using namespace stactiverecord;
 using namespace std;
 Sar_Dbi * Sar_Dbi::dbi;
-/*
-class Person : public Record {
+
+/**
+   This is an example of a one to one object relationship.  A lock has a single key and a key fits
+   a single lock.  The class definitions are more complicated than they need to be - this is 
+   just a typical situation.
+**/
+
+// forward declaration of Key.
+class Key;
+
+class Lock : public Record<Lock> {
 public:
-  std::string firstname;
-  std::string lastname;
-  int age;
-  bool iscool;
-  DateTime dob;
-  Person() : Record("person") { init(); };
-  Person(int id) : Record("person", id) { init(); }
+  static string classname;
+  string name;
+  Key * key;
+  Lock() : Record<Lock>() { init();  };
+  Lock(int id) : Record<Lock>(id) { init(); };
   void init() {
-    get("firstname", firstname, "Unknown");
-    get("lastname", lastname, "Unkown");
-    get("age", age, 0);
-    get("iscool", iscool, false);
-    get("dob", dob, DateTime(1,1,2000));
+    get("name", name, "Unknown"); 
+    if(isset<Key>())
+      getOne<Key>(*key);
   };
   void save() {
-    set("firstname", firstname);
-    set("lastname", lastname);
-    set("age", age);
-    set("iscool", iscool);
-    set("dob", dob);
-    Record::save();
+    set("name", name);
+    // if key variable has been set
+    if(key->has_been_saved())
+      setOne<Key>(*key);
+    Record<Lock>::save();
   };
-  void setBuddy(Person p) { 
-    setOne<Person>(p);
-  };
-  void getBuddy(Person &p) {
-    getOne<Person>(p);
+  void update() {
+    Record<Lock>::update();
+    init();
   };
 };
+string Lock::classname = "lock";
 
-class Test : public Record {
+class Key : public Record<Key> {
 public:
-  Test() : Record("test") {};
-  Test(int id) : Record("test", id) {};
+  static string classname;
+  string name;
+  Lock lock;
+  Key() : Record<Key>() { init(); };
+  Key(int id) : Record<Key>(id) { init(); };
+  void init() {
+    get("name", name, "Unknown"); 
+    if(isset<Lock>())
+      getOne<Lock>(lock);
+  };
+  void save() {
+    set("name", name);
+    // if teacher variable has been set
+    if(lock.has_been_saved())
+      setOne<Lock>(lock);
+    Record<Key>::save();
+  };
+  void update() {
+    Record<Key>::update();
+    init();
+  };
 };
+string Key::classname = "key";
 
-class TestTwo : public Record {
-public:
-  TestTwo() : Record("testtwo") {};
-  TestTwo(int id) : Record("testtwo", id) {};
-};
-*/
+
 int main(int argc, char* argv[]) {
   if(argc != 2) {
-    std::cout << "Usage: ./db_test <scheme://[user[:password]@host[:port]/]database>\n";
+    std::cout << "Usage: ./one_to_many <scheme://[user[:password]@host[:port]/]database>\n";
     return 1;
   }
   Sar_Dbi::dbi = Sar_Dbi::makeStorage(std::string(argv[1]));
-  /*
-  Person p;
-  p.age = 23;
-  p.firstname = "bob";
-  //p.iscool = false;
-  p.save();
-  return 0;
-  Person q;
-  q.setBuddy(p);
-  q.save();
 
-  Person qq(q.id);
-  Person z;
-  qq.getBuddy(z);
-  std::cout << z.firstname << "\n";
-  */
-  /*
-  Person qq(q.id);
-  Person z;
-  qq.getOne<Person>(z);
-  std::cout << z.firstname << "\n";
-  */
+  Key key;
+  key.name = "The Key";
+  key.save();
 
-  /*
-  Test t;
-  t.set("name", "bob");
-  //t.set("fname", "whoops");
-  t.set("age", 70);
-  t.save();
+  Lock lock;
+  lock.name = "The Lock";
+  lock.key = key;
+  lock.save;
 
-  TestTwo tt;
-  //tt.set("name", "fred");
-  tt.set("age", 55);
-  tt.set<Test>(t);
-  tt.save();
+  key.update();
+  cout << "The lock for " << key.name << " is " << key.lock.name << "\n";
 
-  //  ObjGroup<Test> og = Record::find<Test>(Q("age", between(40, 56)) && Q("name", "fred"));
-  //ObjGroup<Test> og = Record::find<Test>(Q("age", nbetween(40, 56)) || Q("name", "fred"));
-  ObjGroup<TestTwo> og = Record::find<TestTwo>(Q("age", between(40, 56)) && Q(hasobject(t)));
-
-  //ObjGroup<Test> og = Record::find<Test>(Q("name", "fred") || (Q("name", "bob") && Q("fname", "whoops")));
-  for(unsigned int i=0; i < og.size(); i++)
-    std::cout << og[i].id << "\n"; 
-
-    
-  Test ttt;
-  ttt.set("name", "asdf");
-  ttt.save();
-
-  ObjGroup<Test> og = Record::find_by<Test>("name", "fred");
-  og.get_ids().dump();
-
-  ObjGroup<Test> og;
-  og << t;
-  og << tt;
-
-  TestTwo f;
-  f.setMany<Test>(og);
-  f.save();
-
-  ObjGroup<Test> secondog;
-  secondog << t;
-  Test x;
-  x.set("name", "yourmamma");
-  x.save();
-  secondog << x;
-
-  f.setMany<Test>(secondog);
-  f.save();
-
-  TestTwo ff(f.id);
-  ObjGroup<Test> oogg = ff.getMany<Test>();
-  string n1, n2;
-  oogg[0].get("name", n1);
-  oogg[1].get("name", n2);
-  cout << n1 << " " << n2 << "\n";
-
-  ff.del<Test>();
-  ff.save();
-  
-  Test f;
-  f.set("one", "blah");
-  f.save();
-
-  f.set("one", 1);  
-  f.save();
-  f.save();
-  f.save();
-
-  f.del();
-  */
-  //cout << "Saved one as integer 1\n";
-
-  //Test t(f.id);
-  //cout << "value should be 1: " << t.get("one") << "\n";
-  /*
-  t.del("one");
-  t.set("one", "four");
-  t.set("one", "five");
-  
-  f.save();
-
-
-  Test t(f.id);
-  t.del("one");
-  t.set("one", 2);
-  t.del("one");
-  t.set("one", 3);
-  t.save();
-  */
-  //cout << "id: " << t.id << "\n";
-  //string value;
-  //t.get("one", value);
-  //cout << "one: " << value << "\n";
-  //t.set("one", "three");
   delete Sar_Dbi::dbi;
   return 0;
 };
